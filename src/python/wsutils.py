93a1350444ed571b622379b7b6b59ddb2ff04f2e
Blake W. Ford <blakewford@gmail.com>
2016-03-20 16:04:53 -0500

    Move python source under a python directory

--- src/python/wsutils.py
+++ src/python/wsutils.py
@@ -11,8 +11,8 @@ global_services_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<UserServi
 global_services_footer = "</UserServices>"
 def generate_user_services_file():
 	all_files = os.listdir(".")
-	for f in infrastructure_files:
-		all_files.remove(f)
+#	for f in infrastructure_files:
+#		all_files.remove(f)
 	services = global_services_header
 	f = open("./LVWSSysAdmin/GetAllUserServices", "w")
 	for s in all_files:
