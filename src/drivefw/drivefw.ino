3038388daeb0faff0c14e51ce272e113989e97ea
blakewford <blakewford@gmail.com>
2016-04-13 22:21:33 -0500

    Update drivefw.ino
    Start merging main Edison application and drivefw

--- src/drivefw/drivefw.ino
+++ src/drivefw/drivefw.ino
@@ -38,3 +38,50 @@ void loop()
 
   delay(10);
 }
+
+/*
+#include <SPI.h>
+#include <WiFi.h>
+
+WiFiClient client;
+int status = WL_IDLE_STATUS;
+IPAddress server(192,168,1,6);
+
+void setup()
+{
+  Serial.begin(9600);
+  while(status != WL_CONNECTED)
+  {  
+    status = WiFi.begin(ssid, pass);
+    delay(200);
+  } 
+}
+
+void http_request(const char* request)
+{
+  if(client.connect(server, 8080))
+  {
+    client.println(request);
+
+    while (client.available())
+    {
+       char c = client.read();
+    }
+  }
+  
+  delay(100);
+
+  if(client.connected())
+  {
+    client.stop();
+  }
+}
+
+void loop() {
+//    http_request("POST /CommandServer/currentJsonCommand HTTP/1.1\nHost: 192.168.1.6:8080\nAccept: */*\nContent-Length: 15\nContent-Type: application/x-www-form-urlencoded\nConnection: close\n\nPLATFORM:EDISON\n");
+//    http_request("GET /CommandServer/currentJsonCommand HTTP/1.1\nHost: 192.168.1.6:8080\nAccept: */*\n");
+}
+*/
+
+
+
